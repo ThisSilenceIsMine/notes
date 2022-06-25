6d26a10a83dc80a0b61a896ee689f8d7b56a33e1
ThisSilenceIsMine <aurakel542@gmail.com>
2022-06-25 03:14:26 +0300

    Basic yoga server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,37 @@
 import { createServer } from '@graphql-yoga/node';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { readFileSync } from 'fs';
+
+import { Resolvers } from '~/graphql/resolvers-types';
+
+const schemaPath = process.cwd() + '/graphql/schema.graphql';
+const typeDefs = readFileSync(schemaPath, 'utf-8');
+
+const resolvers: Resolvers = {
+  Query: {
+    notes: (_, { userId }) => {
+      return [
+        {
+          id: '1',
+          title: 'Note 1',
+          content: 'This is the first note',
+          user: {
+            id: userId + '-user',
+            name: 'John Doe',
+            email: 'foo',
+            password: 'bar',
+          },
+        },
+      ];
+    },
+  },
+};
 
 const server = createServer<{
   req: NextApiRequest;
   res: NextApiResponse;
-}>();
+}>({
+  schema: { typeDefs, resolvers },
+});
 
 export default server;
